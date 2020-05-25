7662e8a8819eb3c91721e73e515dbfa1c6aac040
John <jfelten@pop-os.localdomain>
2020-05-25 10:59:57 -0700

    add test payload

--- main_test.go
+++ main_test.go
@@ -2,7 +2,7 @@
  Copyright 2020 Keyporttech Inc.
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
+ You may obtain a copy of the Licens}ze at
      http://www.apache.org/licenses/LICENSE-2.0
  Unless required by applicable law or agreed to in writing, software
  distributed under the License is distributed on an "AS IS" BASIS,
@@ -23,6 +23,7 @@ import (
 
 const (
   test_secret = "TEST"
+  test_payload = `{ "test123" = "test"}`
 )
 
 func TestValidatePayload(t *testing.T) {
